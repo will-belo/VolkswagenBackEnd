be0e7a2577d3cb9994d130214af6ae399c54421a
William Oliveira <will_belo1@hotmail.com>
2024-06-27 13:55:22 -0300

    Fix/ correção do link

--- app/Services/SinglePassService.php
+++ app/Services/SinglePassService.php
@@ -8,7 +8,7 @@ use RuntimeException;
 class SinglePassService
 {
     public function __construct(
-        private String $base_url = 'http://singlepass-singlepass-1/api' //login.oficinabrasil.com.br
+        private String $base_url = 'https://login.oficinabrasil.com.br/api' //singlepass-singlepass-1
     ){}
 
     public function verify($token)
